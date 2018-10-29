26572e920a506191d5d57fec1fd684f4c3b01920
Cindy Neilson <cindy.neilson@gmail.com>
2018-10-29 15:40:55 -0700

    setting everything up

---
+++ app.js
@@ -0,0 +1,24 @@
+
+
+const addTask = {
+    thingINeedToDo: function() {
+
+    }
+
+};
+
+
+
+// I'm adding this way too early
+// I am supposed to wait until I've got basic functionality before worrying about this
+
+// function customParser(key, value) {
+    // if(key !== 'due') return value;
+    // return new Date(value);
+// }
+
+// later in your code, you can use JSON.parse like:
+// const saved = localStorage.getItem('todos');
+// if(saved) {
+    // todos = JSON.parse(saved, customParser);
+// }
---
+++ todo-list.js
@@ -0,0 +1,5 @@
+/* exported words
+eslint-disable no-console */
+'use strict';
+
+var tasks = ['walk the dog', 'feed the dog', 'throw the ball for the dog', 'go to the grocery store', 'laundry', 'pick up drycleaning', 'take the car in for oil change'];
